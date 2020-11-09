020c16484bce34de1c28e219a18fbe29559bffc9
YodaNangoy <s11910026student.unklab.ad.id>
2020-11-09 07:03:05 -0800

    Web Design

--- Js/index.js
+++
@@ -1,8 +0,0 @@
-let berat = prompt("Masukan Berat : ");
-console.log("Berat anda adalah" + berat);
-
-let tinggi = prompt("Masukan Tinggi : ");
-console.log("Tinggi anda adalah "+ tinggi);
-
-let bmi = berat / (tinggi * tinggi);
-console.log("BMI = " + bmi);
---
+++ Pertemuan 23/index.js
@@ -0,0 +1,4 @@
+let berat=prompt ("Enter the Weight :") ;
+let tinggi=prompt ("Enter Height :");
+let bmi=berat/(tinggi*tinggi);
+console.log("BMI =" + bmi );
